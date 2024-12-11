0b3bd8b709c1aa78442408c8d0e00cc03a56f0ca
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-12-11 15:13:52 +0545

    Add project files.

---
+++ Basic/Car.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Basic
+{
+    public class Car
+    {
+        public string Type;
+        public string Name;
+    }
+}
---
+++ Basic/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Basic;
+
+Car car = new Car();
+
+car.Type = "SUV";
+car.Name = "Land Rover";
+
+Console.WriteLine(car.Type);
+Console.WriteLine(car.Name);
