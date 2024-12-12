fcc7fba41fcbce977a84f3daf1c2adf6cd5ec748
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-12-12 11:28:37 +0545

    Basic project for calculator.

---
+++ Calculator/Calc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calc
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
+        public int Add() 
+        {
+            return Number1 + Number2;
+        }
+
+        public int Difference()
+        {
+            return Number1 - Number2;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using Calculator;
+
+Calc calc = new Calc();
+
+Console.Write("Enter the first number: ");
+calc.Number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+calc.Number2 = int.Parse(Console.ReadLine());
+
+int Sum = calc.Add();
+
+Console.WriteLine($"The sum is {Sum}.");
+
+int Diff = calc.Difference();
+
+Console.WriteLine($"The difference is {Diff}.");
