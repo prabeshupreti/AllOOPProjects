fa7d8fcb82ad0bcbe95ba89b32b5adb7ef45ade9
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-12-13 13:27:31 +0545

    Added a code to manually throw the errors.

---
+++ ThrowAndCatch/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThrowAndCatch
+{
+    public class Calculator
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
+        public int Add()
+        {
+            return Number1 + Number2;
+        }
+
+        public int Difference()
+        {
+            return Math.Abs(Number1 - Number2);
+        }
+
+        public int Product()
+        {
+            return Number1 * Number2;
+        }
+
+        public int Division()
+        {
+            if (Number2 == 0) 
+            {
+                throw new DivideByZeroException();
+            }
+
+            return Number1 / Number2;
+        }
+    }
+}
