bc19f24748bfbf2acfc37444c810f230e6d09bc8
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-12-13 10:51:54 +0545

    Added code to demonstrate the documentation of the code.

---
+++ Documentation/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documentation
+{
+
+    /// <summary>
+    /// A Calculator class to perform basic mathematic calculation.
+    /// </summary>
+    public class Calculator
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
+        /// <summary>
+        /// A method to add two numbers.
+        /// </summary>
+        /// <returns>The sum of two numbers.</returns>
+        public int Add()
+        {
+            return Number1 + Number2;
+        }
+
+        /// <summary>
+        /// A method to find the difference between the numbers.
+        /// </summary>
+        /// <returns>The sum of difference between two numbers.</returns>
+        public int Difference()
+        {
+            return Math.Abs(Number1 - Number2);
+        }
+
+        /// <summary>
+        /// A method to find the product of the two numbers.
+        /// </summary>
+        /// <returns>The product of two numbers.</returns>
+        public int Product()
+        {
+            return Number1 * Number2;
+        }
+        /// <summary>
+        /// A method to find the division of a number by another number. 
+        /// </summary>
+        /// <returns>The divised number.</returns>
+        public int Division()
+        {
+            return Number1 / Number2;
+        }
+    }
+}
--- Documentation/Program.cs
+++ Documentation/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Documentation;
+
+Calculator calculator = new Calculator();
+
+Console.Write("Enter the first number: ");
+calculator.Number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+calculator.Number2 = int.Parse(Console.ReadLine());
+
+int Sum = calculator.Add();
+
+Console.WriteLine($"The sum is {Sum}.");
+
+int Diff = calculator.Difference();
+
+Console.WriteLine($"The difference is {Diff}.");
