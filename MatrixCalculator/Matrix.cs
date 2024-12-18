aa705b5c08e902909b3a595783519cc61f626bcc
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-12-18 14:58:49 +0545

    Using operator overloading to perform matrix addition.

--- MatrixCalculator/Matrix.cs
+++ MatrixCalculator/Matrix.cs
@@ -10,5 +10,25 @@ namespace MatrixCalculator
         {
             M = new int[FirstDimension, SecondDimension];
         }
+
+        public static Matrix operator +(Matrix Left, Matrix right) 
+        {
+
+            if (Left.M.GetLength(0) != right.M.GetLength(0) && Left.M.GetLength(1) == right.M.GetLength(1)) 
+            {
+                throw new ArgumentException("Matrices dimension are not equal");
+            }
+
+            Matrix MatrixSum = new Matrix(Left.M.GetLength(0), Left.M.GetLength(1));
+
+            foreach (int i in Enumerable.Range(0, MatrixSum.M.GetLength(0)))
+            {
+                foreach (int j in Enumerable.Range(0, MatrixSum.M.GetLength(1)))
+                {
+                    MatrixSum.M[i, j] = Left.M[i, j] + right.M[i, j];
+                }
+            }
+            return MatrixSum;
+        }
     }
 }
--- MatrixCalculator/Program.cs
+++ MatrixCalculator/Program.cs
@@ -4,3 +4,13 @@ using MatrixCalculator;
 Matrix matrixA = new Matrix(2, 2);
 
 Matrix matrixB = new Matrix(2, 2);
+Matrix Sum = matrixA + matrixB;
+
+for (int i = 0; i < Sum.M.GetLength(0); i++)
+{
+    for (int j = 0; j < Sum.M.GetLength(1); j++)
+    {
+        Console.Write($"   {Sum.M[i, j]}");
+    }
+    Console.WriteLine();
+}
