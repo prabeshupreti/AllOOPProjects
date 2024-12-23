97377dc5cf3117c18a522bf2ee5ac02443de9735
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2024-12-23 09:14:20 +0545

    Improved code based on MatrixCalculator project using operator overloading.

---
+++ MatrixCalculatorImproved/Matrix.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixCalculatorImproved
+{
+    public class Matrix
+    {
+        private int[,] M;
+
+        public Matrix(int FirstDimension, int SecondDimension)
+        {
+            M = new int[FirstDimension, SecondDimension];
+        }
+
+        public int this[int row, int col]
+        {
+            get
+            {
+                return M[row, col];
+            }
+            set
+            {
+                M[row, col] = value;
+            }
+        }
+
+        public int GetLength(int Dimension)
+        {
+            return M.GetLength(Dimension);
+        }
+
+        public static Matrix operator +(Matrix Left, Matrix Right)
+        {
+            if (Left.M.GetLength(0) != Right.M.GetLength(0) && Left.M.GetLength(1) != Right.M.GetLength(1))
+            {
+                throw new ArgumentException("Matrices dimension are not equal");
+            }
+
+            Matrix MatrixSum = new(Left.M.GetLength(0), Left.M.GetLength(1));
+
+            foreach (int i in Enumerable.Range(0, MatrixSum.M.GetLength(0)))
+            {
+                foreach (int j in Enumerable.Range(0, MatrixSum.M.GetLength(1)))
+                {
+                    MatrixSum.M[i, j] = Left.M[i, j] + Right.M[i, j];
+                }
+            }
+            return MatrixSum;
+        }
+    }
+}
---
+++ MatrixCalculatorImproved/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using MatrixCalculatorImproved;
+
+Matrix matrixA = new Matrix(2, 2);
+
+Matrix matrixB = new Matrix(2, 2);
+
+Console.WriteLine("Enter the matrix A numbers");
+foreach (int i in Enumerable.Range(0, matrixA.GetLength(0)))
+{
+    foreach (int j in Enumerable.Range(0, matrixA.GetLength(1)))
+    {
+        matrixA[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Enter the matrix B numbers");
+foreach (int i in Enumerable.Range(0, matrixB.GetLength(0)))
+{
+    foreach (int j in Enumerable.Range(0, matrixA.GetLength(1)))
+    {
+        matrixB[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Matrix MatrixSum = matrixA + matrixB;
+
+Console.WriteLine();
+foreach (int i in Enumerable.Range(0, MatrixSum.GetLength(0)))
+{
+    foreach (int j in Enumerable.Range(0, MatrixSum.GetLength(1)))
+    {
+        Console.WriteLine(MatrixSum[i, j]);
+    }
+}
