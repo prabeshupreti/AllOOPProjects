1d3e2a292ff05d69efd3089c60d438da7e63936f
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-01-02 14:38:15 +0545

    Database connetivity setup.

---
+++ DatabaseOperations/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseOperations
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string ConnectionString = "server=localhost;uid=root;database=OfficeDemoApp";
+            optionsBuilder.UseMySql(ConnectionString, ServerVersion.AutoDetect(ConnectionString));
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
