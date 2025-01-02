0338067fb649afad7266074e6078f46a635b3df6
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-01-02 14:39:09 +0545

    A sample code to add a new record.

---
+++ DatabaseOperations/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using DatabaseOperations;
+
+
+AppDbContext appDbContext = new AppDbContext();
+Department department = new Department();
+
+Console.WriteLine("Enter the department name: ");
+
+department.Name = Console.ReadLine();
+
+appDbContext.Departments.Add(department);
+appDbContext.SaveChanges();
