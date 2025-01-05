3aa3f96b8d691192dc452f02afcbee0f8e09811d
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-01-05 08:39:07 +0545

    Minor changes and fixes.

--- DatabaseOperations/Migrations/AppDbContextModelSnapshot.cs
+++ DatabaseOperations/Migrations/AppDbContextModelSnapshot.cs
@@ -35,7 +35,7 @@ namespace DatabaseOperations.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Departments");
+                    b.ToTable("Departments", (string)null);
                 });
 #pragma warning restore 612, 618
         }
