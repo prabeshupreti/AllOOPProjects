c06ee4ad708f04bb4801f36c73b4d98cef71be41
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-01-05 08:39:38 +0545

    Added switch cases to perform CRUD operations.

--- DatabaseOperations/Program.cs
+++ DatabaseOperations/Program.cs
@@ -4,10 +4,70 @@ using DatabaseOperations;
 int Choice;
 DepartmentService departmentService = new DepartmentService();
 
-Console.WriteLine("Select your actions:\n");
+Beginning:
+
+Console.WriteLine("\nSelect your actions:\n");
 Console.WriteLine("1. Add a department");
 Console.WriteLine("2. View all departments");
-Console.Write("\n What is your choice ?:");
+Console.WriteLine("3. Update a department");
+Console.WriteLine("4: Delete a department");
+Console.Write("\n What is your choice ?: ");
 Choice = int.Parse(Console.ReadLine());
 
-//Complete the code with switch case how ever you want to.
+if (!(Choice == 1 || Choice == 2 || Choice == 3 || Choice == 4))
+{
+    Console.Clear();
+    Console.WriteLine("Wrong choice selected.\n");
+    goto Beginning;
+}
+
+Console.WriteLine();
+switch (Choice)
+{
+    case 1:
+        {
+            Department department = new Department();
+            Console.Write("Enter the department name you want to add: ");
+            department.Name = Console.ReadLine();
+            departmentService.AddDepartment(department);
+            Console.WriteLine("The department record is added.");
+            break;
+        }
+
+    case 2:
+        var departments = departmentService.ViewAllDepartments();
+        Console.WriteLine("Below are all the departments: ");
+
+        foreach (var departmentItem in departments)
+        {
+            Console.WriteLine($"\nId:{departmentItem.Id} Name:{departmentItem.Name}");
+        }
+        break;
+
+    case 3:
+        {
+            Department department = new Department();
+            Console.Write("Enter the id of the department record you want to update: ");
+            department.Id = int.Parse(Console.ReadLine());
+            Console.Write("Enter the updated name: ");
+            department.Name = Console.ReadLine();
+            departmentService.UpdateDepartment(department);
+            Console.WriteLine("The department record is updated.");
+        }
+        break;
+
+    case 4:
+        Console.Write("Enter the id of the department record you want to delete: ");
+        int Id = int.Parse(Console.ReadLine());
+        departmentService.DeleteDepartment(Id);
+        Console.WriteLine("The department record is updated.");
+        break;
+}
+
+Console.WriteLine("\nDo you want perform more operations? (y/n):");
+string yn = Console.ReadLine();
+
+if (yn == "Y" || yn == "y") 
+{
+    goto Beginning;
+}
