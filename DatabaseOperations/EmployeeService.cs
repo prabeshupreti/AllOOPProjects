7f3d47e70e504bce1b21f1702357e9ec00245c3a
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-01-06 12:59:38 +0545

    Added Employee Service with CRUD features.

---
+++ DatabaseOperations/EmployeeService.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseOperations
+{
+    public class EmployeeService
+    {
+        public void AddEmployee(Employee employee)
+        {
+            AppDbContext appDbContext = new AppDbContext();
+            appDbContext.Employees.Add(employee);
+            appDbContext.SaveChanges();
+        }
+
+        public Employee[] ViewAllEmployees()
+        {
+            AppDbContext appDbContext = new AppDbContext();
+            var employees = appDbContext.Employees.Include(x => x.Department).ToArray();
+            return employees;
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            AppDbContext appDbContext = new AppDbContext();
+            var employeeValue = appDbContext.Employees
+                .Include(x => x.Department)
+                .Where(x => x.Id == id).FirstOrDefault();
+
+            return employeeValue;
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            AppDbContext appDbContext = new AppDbContext();
+            appDbContext.Employees.Update(employee);
+            appDbContext.SaveChanges();
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            AppDbContext appDbContext = new AppDbContext();
+            var departments = appDbContext.Employees.Remove(employee);
+            appDbContext.SaveChanges();
+        }
+    }
+}
