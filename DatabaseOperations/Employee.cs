68595e4217f4d3cab517004a77db01de7019b03f
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-01-06 12:55:10 +0545

    Added Employee class for tables.

---
+++ DatabaseOperations/Employee.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DatabaseOperations
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public float Salary { get; set; }
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
