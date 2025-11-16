206b6588d60014c1640b7d38b8d2747a0a9282a4
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 14:46:10 +0545

    Modified code.

--- Calculator/Calc.cs
+++ Calculator/Calc.cs
@@ -21,12 +21,27 @@ namespace Calculator
             return A + B + C;
         }
 
+        public int Add(int A, int B, params int[] Numbers)
+        {
+            return A + B + Numbers.Sum();
+        }
+
         public float Add(float A, float B)
         {
             return A + B;
         }
 
+        public float Add(float A, float B, params int[] Numbers)
+        {
+            return A + B + Numbers.Sum();
+        }
+
         public long Add(long A, long B)
+        {
+            return A + B;
+        }
+
+        public long Add(long A, long B, params long[] Numbers)
         {
             return A + B;
         }
