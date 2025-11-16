0f5d0f66908b0b44b96ce30553b5edfb90e26cb1
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 14:46:23 +0545

    Overloading bitwise operators.

---
+++ BitwiseOperatorOverloadExample/Flags.cs
@@ -0,0 +1,48 @@
+﻿
+namespace BitwiseOperatorOverloadExample
+{
+    public class Flags
+    {
+        public int Value { get; set; }
+
+        public Flags(int value)
+        {
+            Value = value;
+        }
+
+        // Bitwise AND
+        public static Flags operator &(Flags a, Flags b)
+        {
+            return new Flags(a.Value & b.Value);
+        }
+
+        // Bitwise OR
+        public static Flags operator |(Flags a, Flags b)
+        {
+            return new Flags(a.Value | b.Value);
+        }
+
+        // Bitwise XOR
+        public static Flags operator ^(Flags a, Flags b)
+        {
+            return new Flags(a.Value ^ b.Value);
+        }
+
+        // Bitwise NOT
+        public static Flags operator ~(Flags a)
+        {
+            return new Flags(~a.Value);
+        }
+
+        public static implicit operator Flags(int value)
+        {
+            return new Flags(value);
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+
+}
---
+++ BitwiseOperatorOverloadExample/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using BitwiseOperatorOverloadExample;
+
+var f1 = new Flags(2);  // 2 in integer
+Flags f2 = 3;   // 3 in integer || no need to write Flags f2 = new Flags(3);
+
+Console.WriteLine(f1 & f2);  
+Console.WriteLine(f1 | f2);  
+Console.WriteLine(f1 ^ f2);  
+Console.WriteLine(~f1);
