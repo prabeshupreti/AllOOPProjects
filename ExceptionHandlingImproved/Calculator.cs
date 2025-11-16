93a840613b4994a6028dbeec76092b0909944251
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 11:26:17 +0545

    Code to write better excption handling.

---
+++ ExceptionHandlingImproved/Calculator.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ExceptionHandlingImproved
+{
+    public class Calculator
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
+        public int Add()
+        {
+            return Number1 + Number2;
+        }
+
+        public int Difference()
+        {
+            return Math.Abs(Number1 - Number2);
+        }
+
+        public int Product()
+        {
+            return Number1 * Number2;
+        }
+
+        public int Division()
+        {
+            if (Number2 == 0) 
+            {
+                throw new DivideByZeroException();
+            }
+
+            return Number1 / Number2;
+        }
+    }
+}
---
+++ ExceptionHandlingImproved/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using ExceptionHandlingImproved;
+
+Calculator calculator = new Calculator();
+
+Console.Write("Enter a number: ");
+calculator.Number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter another number: ");
+calculator.Number2 = int.Parse(Console.ReadLine());
+
+try
+{
+    int Diff = calculator.Division();
+
+    Console.WriteLine($"The division is {Diff}.");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (IOException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally 
+{
+    Console.WriteLine("Execution completed.");
+}
