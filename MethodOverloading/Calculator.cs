379c74b7f91c7af0293fa6ad48ba2f374685d7a5
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-11-16 11:26:45 +0545

    Moethod overloading example.

---
+++ MethodOverloading/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class Calculator
+    {
+
+
+        public Calculator()
+        {
+        }
+
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Add(int a, int b, params int[] Numbers)
+        {
+            return a + b + Numbers.Sum();
+        }
+
+        public float Add(float a, float b, params float[] Numbers)
+        {
+            return a + b + Numbers.Sum();
+        }
+
+        public double Add(double a, double b, params double[] Numbers)
+        {
+            return a + b + Numbers.Sum();
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public float Add(float a, float b)
+        {
+            return a + b;
+        }
+
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+using MethodOverloading;
+
+Calculator calculator = new Calculator();
+
+calculator.Add(5, 10, 5, 6);
